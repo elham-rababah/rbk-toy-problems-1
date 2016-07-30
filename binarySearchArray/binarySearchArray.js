f26cbc5fef7810f5885c93d7f5d64842f67d4cbb
elham-rababah <elham-rababah@yahoo.com>
2016-07-30 10:13:21 +0300

    deepEquals

--- binarySearchArray/binarySearchArray.js
+++ binarySearchArray/binarySearchArray.js
@@ -9,22 +9,23 @@
  * var index = binarySearch([1, 2, 7, 15, 27], 27);
  * console.log(index); // 4
  */
-var index=0;
+var index;
 var binarySearch = function (array, target) {
 	
 	// we must return index;
 	var mid = Math.floor(array.length / 2);
+	index =mid;
 	  if(array[mid]=== target){
 	  	
-	  	console.log(index-1);
+	  	console.log(index);
 	  	return mid
 	  }
      else if (array[mid]>target && array.length >1 ){
      	      index=index-1;
-         return  binarySearch ((array.splice(0,mid)),target)
+         return  binarySearch((array.splice(0,mid)),target)
      }else if (array[mid]< target && array.length >1 ){
-     	        index=index+1;
-         return binarySearch ((array.splice(mid+1)),target)
+     	        index=index+mid;
+         return binarySearch((array.splice(mid+1)),target)
      }else{
      	return -1
      }
--- deepEquality/deepEquality.js
+++ deepEquality/deepEquality.js
@@ -12,4 +12,31 @@
   *
   */
 var deepEquals = function(obj1, obj2){
+  for (var key in obj1){
+    console.log(key)
+    if((obj2[key])===undefined){
+      return false;
+    }else{
+      if(typeof(obj1[key])!==typeof(obj2[key])){
+        return false
+      }else{
+        if(typeof(obj1[key])!== "object"){
+          if(obj1[key]!== obj2[key]){
+            return false 
+        }else{
+          //search in arr
+          if(Array.isArray(obj1[key])){
+            for (var i = 0; i < obj1[key].length; i++) {
+              if(obj1[key][i]!== obj2[key][i]){
+                return false
+              }
+            }
+          }else{
+            return deepEquals(obj1[key],obj2[key])
+          }
+        }
+      }
+    }
+  }
+  return true;
 };
