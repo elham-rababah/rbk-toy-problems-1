f0579b4fbcc813f5489aa01de4e976028a81ea3f
elham-rababah <elham-rababah@yahoo.com>
2016-08-02 10:10:11 +0300

    infite looooop

--- telephoneLetters/telephoneLetters.js
+++ telephoneLetters/telephoneLetters.js
@@ -27,3 +27,47 @@
   *  Why not filter your results to only return words contained in that file?
   *
   */
+  var telephoneWords=function(num){
+    var arr=[[0],[1],['a','d','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','v','u'],['w','x','y','z']]
+    var collofstr=[]
+    console.log("eeeeeeee")
+    var first=Number(num[0]);
+    var second=Number(num[1]);
+    var third=Number(num[2]);
+    var fourth=Number(num[3]);
+    console.log("edrfghjkl;",first)
+    for (var i = 0; i <arr[first].length ; i++) {
+      var str= arr[first][i]
+      for (var y = 0; y < arr[second].length; y++) {
+        str=str+arr[second][y]
+        for (var x = 0; x < third.length; x++) {
+          str=str+arr[third][x]
+          for (var e = 0; e < fourth.length; e++) {
+            str=str+arr[fourth][e];
+            collofstr.push(str)
+            //str=" "
+          }
+        }
+
+        
+      }
+    }
+   return collofstr;
+  }
+
+  /*var telephoneWords=function(num){
+    var arr=[[],[],['a','d','c'],['d','e','f'],['g','h','i'],['j','k','l'],['m','n','o'],['p','q','r','s'],['t','v','u'],['w','x','y','z']]
+    var collofstr=[]
+    for (var i = 0; i < num.length; i++) {
+      var str
+      if (num[i] !== 1 || num[i] !== 0){
+        str=str+num[i]
+      }else{
+        for (var i = 0; i <arr[Number(num[i])].length; i++) {
+          str=str+arr[Number(num[i])][i]
+        }
+        collofstr.push(str)
+      }
+    }
+   return str;
+  }*/
