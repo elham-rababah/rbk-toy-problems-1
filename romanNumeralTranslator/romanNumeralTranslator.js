f879950b85260d031f16495369c700172c20569e
elham-rababah <elham-rababah@yahoo.com>
2016-08-10 09:47:38 +0300

    romanNumeralTranslator.js

--- romanNumeralTranslator/romanNumeralTranslator.js
+++ romanNumeralTranslator/romanNumeralTranslator.js
@@ -26,6 +26,21 @@ var DIGIT_VALUES = {
   M: 1000
 };
 
+
 var translateRomanNumeral = function(romanNumeral){
+	var value=0
+	for (var i = 0; i < romanNumeral.length; i++) {
+		if(romanNumeral[i]== 'I'||romanNumeral[i]=='V'||romanNumeral[i]=='X' || romanNumeral[i]=='L' ||romanNumeral[i]=='C'||romanNumeral[i]=='D'){
+           if(DIGIT_VALUES[romanNumeral[i]]>DIGIT_VALUES[romanNumeral[i-1]]){
+           		value=value+DIGIT_VALUES[romanNumeral[i]]- DIGIT_VALUES[romanNumeral[i-1]]- DIGIT_VALUES[romanNumeral[i-1]];
+           	}
+           else{
+           	value=value+DIGIT_VALUES[romanNumeral[i]]
+           }
+		}else{
+			value=null
+		}
+	}
+	return value;
 
 };
