c1aa1c0a228a447d89bec63880567b76d389e5e4
elham-rababah <elham-rababah@yahoo.com>
2016-08-17 09:41:59 +0300

    tic-tac-toe/ticTacToe.js

--- tic-tac-toe/ticTacToe.js
+++ tic-tac-toe/ticTacToe.js
@@ -31,5 +31,51 @@ ticTacToe(solvedBoard);//should return 1
 */
 
 function ticTacToe(board){
+	var flage=true;
+
+ 	
+ 		if(board[0][0]===1 ){
+ 			if(board[0][1]===1 && board[0][2]===1 ){
+ 				return 1;
+ 			}
+ 			if(board[1][0]===1 && board[2][0]===1 ){
+ 				return 1;
+ 			}
+ 			if(board[1][1]===1 && board[2][2]===1 ){
+ 				return 1;
+ 			}
+
+
+ 		}
+ 		if(board[0][0]===2 ){
+ 			if(board[0][1]===2 && board[0][2]===2 ){
+ 				return 2;
+ 			}
+ 			if(board[1][0]===2 && board[2][0]===2 ){
+ 				return 2;
+ 			}
+ 			if(board[1][1]===2&& board[2][2]===2 ){
+ 				return 2;
+ 			}
+ 		}
+ 		if(board[0][1] ===1 && board[1][1] ===1 && board[2][1] ===1  ){
+ 			return 1
+ 		}
+ 		if(board[0][1] ===2&& board[1][1] ===2 && board[2][1] ===2  ){
+ 			return 2
+ 		}
+
+ 		if(board[0][2] ===1 && board[1][2] ===1 && board[2][2] ===1  ){
+ 			return 1
+ 		}
+ 		if(board[0][2] ===2&& board[1][2] ===2 && board[2][2] ===2  ){
+ 			return 2
+ 		}
+ 		return -1;
+ 
 
 };
+
+
+
+
