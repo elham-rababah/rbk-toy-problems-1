8544296f1b5805a1fb8d58492a82b2bf41bd055b
elham-rababah <elham-rababah@yahoo.com>
2016-08-23 10:08:20 +0300

    powerOfTwo

--- powerOfTwo/powerOfTwo.js
+++ powerOfTwo/powerOfTwo.js
@@ -2,8 +2,16 @@
 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 Calculate the sum of the digits of the number 2^1000?
 Hint the answer is 1366.
+//1.0715086071862673e+301
 */
 
-var powerOfTwo = function(){
+var powerOfTwo = function(num){
+	var power=Math.pow(2,num);
+	console.log(power)
+	power=power.toString().split("").splice(power.indexOf("."),1)
+	power=power.splice(power.indexOf("."),1)
+	/*power=power.splice(power.indexOf("."),1);
+	power=power.splice(power.indexOf("e+"),2)*/
+	console.log(power)
   return 0;
 }
