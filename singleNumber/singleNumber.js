4dce8171e4d4b8f167bd9b4bf1acb4b9aefd2bcc
elham-rababah <elham-rababah@yahoo.com>
2016-08-24 09:59:29 +0300

    singleNumber

--- singleNumber/singleNumber.js
+++ singleNumber/singleNumber.js
@@ -15,7 +15,12 @@ singleNumber(arr);//should return 6 because it only appear once
 */
 
 function singleNumber(arr){
-
+ arr.sort()
+ for (var i = 0; i < arr.length; i=i+2) {
+ 	if(arr[i]!==arr[i+1]){
+ 		return arr[i]
+ 	}
+ }
 }
 
 /*
@@ -33,7 +38,13 @@ twoSum(nums,9); //should return [0,1] because nums[0] + nums[1] = 2 + 7 = 9;
 */
 
 function twoSum(nums, target){
-
+ for (var i = 0; i < nums.length; i++) {
+ 	for (var y = i+1; y < nums.length; y++) {
+ 		if(nums[y]+nums[i] === target){
+ 			return[nums[i],nums[y]]
+ 		}
+ 	}
+ }
 }
 
 /*
@@ -48,6 +59,28 @@ countAllNumbers(2); //should return 91 because there are 91 numbers that have un
 */
 
 function countAllNumbers(n){
+	var end=Math.pow(10,n)
+	var sum=0;
+	for (var i = 1; i < end; i++) {
+		
+		var arr=i.toString().split("")
+		var flage=false
+		for (var y = 0; y < arr.length; y++) {
+			console.log(arr[y],arr[y+1])
+			if(arr[y]!==arr[y+1]){
+				flage=true;
+				break;
+			}
+		}
+		//console.log(flage);
+		if(flage==true){
+			//console.log(i)
+			sum=sum+i
+		}
+		
+		//console.log(flage);
+	}
+	return sum;
 
 };
 
